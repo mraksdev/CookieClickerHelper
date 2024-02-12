db9ca8109f8afb5f955fecfc346da4667f989d85
Alexandr <55324099+mraksdev@users.noreply.github.com>
2024-02-12 14:22:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-var autoclick = setInterval(function(){
-    try {
-        Game.lastClick -= 1000;
-        Game.ClickCookie();
-    } catch (err) {
-        console.error('Stopping program');
-        clearInterval(autoclick);
-    }
-}, 0.001);
+var isClicking = false
 
-const autoGC = setInterval(
-    () => {
-        for (let h = 0; h < Game.shimmers; h++) {
-            if (Game.shimmers[h].type=="golden") {
-                Game.shimmers[h].pop()
-            }
-        }
-    }, 1000);   
+function getBtnColor() {
+  var toggleColor;
+  if (isClicking) {
+    toggleColor = "green";
+  } else {
+    toggleColor = "red";
+  }
+  return toggleColor; 
+}
+
+function getBtnText() {
+  var toggleText;
+  if (isClicking) {
+    toggleText = "Кликер ON";
+  } else {
+    toggleText = "Кликер OFF";
+  }
+  return toggleText; 
+}
+
+function startClicking() {
+  clickTimer = setInterval(function() {
+    Game.ClickCookie();
+  }, 4);
+}
+
+function stopClicking() {
+  clearInterval(clickTimer);
+}
+
+function switchClicker() {
+  if (isClicking) {
+      startClicking();
+  } else stopClicking();
+}
+
+function toggleClicker() {
+  const Clicker = document.querySelector('#clicker');
+  isClicking = !isClicking;
+  switchClicker();
+  Clicker.style.color = getBtnColor();
+  Clicker.textContent = getBtnText();
+  
+}
+
+function addButton() {
+  const topBar = document.querySelector('#topBar');
+  let toggleElement = document.createElement('div');
+  toggleElement.id = "clicker";
+  toggleElement.style.color = "red";
+  toggleElement.style.cursor = "pointer";
+  toggleElement.textContent = "Кликер OFF";
+  toggleElement.onclick = toggleClicker;
+  topBar.appendChild(toggleElement);
+}
+
+addButton();
